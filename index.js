dd2280daf8d802c59daba50313f3643c6a652652
mmatlou22 <mmatloumodiba9@gmail.com>
2024-07-13 15:13:08 +0200

    Updated HTML & JS
    HTML
    -Added a new id
    
    JS
    -Added API url and key
    -Programmed new functions for interactivity

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+function displayApiCity(response){
+    const tempValue=document.getElementById("tempValue");
+    tempValue.textContent=(Math.floor(response.data.temperature.current))
+    const cityInput=document.getElementById("cityInput")
+    cityInput.textContent=response.data.city;
+}
+
+
+function apiCity(city){
+ let apiKey="0cb149e913a89ff1dbc6ab7o6ft5fdf4";
+ let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+ axios.get(apiUrl).then(displayApiCity);
+}
+
 
 function displayNewCurrent(event){
  event.preventDefault();
  
  const searchInput=document.getElementById("searchInput");
- const cityInput = document.getElementById("cityInput");
- cityInput.textContent=searchInput.value
+
+ apiCity(searchInput.value)
+
 }
 
 const searchContainer=document.querySelector("#searchContainer")
