b477bb8c2cd8549fb84f86b9b20f9e381aa3ca57
mmatlou22 <mmatloumodiba9@gmail.com>
2024-07-14 09:40:54 +0200

    Updated JS file

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-function displayApiCity(response){
-    const tempValue=document.getElementById("tempValue");
-    const cityInput = document.getElementById("cityInput");
-    const tempDescription=document.getElementById('tempDescription')
-    const tempHumidity=document.getElementById("tempHumidity");
-    const tempWind=document.getElementById("tempWind");
-    let  cityTime=document.getElementById("cityTime");
-    let date = new Date(response.data.time * 1000);
-    let iconImage=document.getElementById("iconImage");
+function cityApiResponse(response) {
+  console.log(response);
+  const tempValue = document.getElementById("tempValue");
+  const tempHumidity = document.getElementById("tempHumidity");
+  const tempWind = document.getElementById("tempWind");
+  const tempDescription = document.getElementById("tempDescription");
+  let cityTime = document.getElementById("cityTime");
+  let date = new Date(response.data.time * 1000);
+  let iconImage = document.getElementById("iconImage");
+  console.log(response);
 
-    
-    console.log(response)
-    tempValue.textContent=(Math.floor(response.data.temperature.current));
-   cityInput.textContent=response.data.city;
-    tempDescription.textContent=response.data.condition.description;
-    tempHumidity.textContent=`${response.data.temperature.humidity}%`;
-    tempWind.textContent=`${response.data.wind.speed}km/h`;
-    cityTime.textContent=displayDate(date)
-    iconImage.innerHTML =`<img src="${response.data.condition.icon_url}" class="tempIcon" id="tempIcon">`;
+  tempValue.textContent = Math.floor(response.data.temperature.current);
+  tempHumidity.textContent = `${response.data.temperature.humidity}%`;
+  tempWind.textContent = `${response.data.wind.speed}km/h`;
+  tempDescription.textContent = response.data.condition.description;
+  cityTime.textContent = updatedTime(date);
+  iconImage.innerHTML = `<img src="${response.data.condition.icon_url}" class="tempIcon" id="tempIcon"/>`;
+  console.log(iconImage);
 }
-function displayDate(date){
-    let hours=date.getHours()
-    let minutes=date.getMinutes()
-   
-
-    if(minutes <10){
-    
-        minutes=`0${minutes}`
-    }
-    
-    
-    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let day=days[date.getDay()]
-    let timeString = `${day} ${hours}:${minutes}`;
-  console.log(timeString)
-    return timeString
+function updatedTime(date) {
+  let hour = date.getHours();
+  let minutues = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let timeString = `${day} ${hour}:${minutues}`;
+  return timeString;
 }
 
-function apiCity(city){
- let apiKey="0cb149e913a89ff1dbc6ab7o6ft5fdf4";
- let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
- axios.get(apiUrl).then(displayApiCity);
+function applyApiContent(city) {
+  let apiKey = `0cb149e913a89ff1dbc6ab7o6ft5fdf4`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(cityApiResponse);
 }
 
+function displayCity(event) {
+  event.preventDefault();
 
-function displayNewCurrent(event){
- event.preventDefault();
- 
- const searchInput=document.getElementById("searchInput");
-
- apiCity(searchInput.value)
-
+  const searchInput = document.getElementById("searchInput");
+  const cityInput = document.getElementById("cityInput");
+  cityInput.textContent = searchInput.value;
+  applyApiContent(searchInput.value);
 }
 
-const searchContainer=document.querySelector("#searchContainer")
-searchContainer.addEventListener("submit" ,displayNewCurrent)
+const searchContainer = document.getElementById("searchContainer");
+searchContainer.addEventListener("submit", displayCity);
